583d128d71ccf803ea8348ba5daf663153578883
daskiro514 <luckybuzstar@gmail.com>
2021-10-20 16:11:12 +0900

    updated

--- routes/api/prediction.js
+++ routes/api/prediction.js
@@ -164,7 +164,7 @@ const sendCustomersEmailGoodMatches = async (predictions) => {
 
     for (var predictionIndex = 0; predictionIndex < predictionsForEmail.length; predictionIndex++) {
       var prediction = predictionsForEmail[predictionIndex]
-      emailText += (prediction.firstTeam + ' - ' + prediction.secondTeam + ' | ' + prediction.league + ' | ' + prediction.date + ' | ' + 'Style: Over / Under 2.5 | Risk: ' + prediction.risk + '\n\n')
+      emailText += (prediction.firstTeam + ' - ' + prediction.secondTeam + ' | ' + prediction.league + ' | ' + String(prediction.date).slice(0, 10) + ' | ' + 'Style: Over / Under 2.5 | Risk: ' + prediction.risk + '\n\n')
     }
 
     for (var userIndex = 0; userIndex < users.length; userIndex++) {
